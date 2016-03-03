94b0d502872d76c465b1caa9d9f4f42e99d47065
Adrian Aichner <adrian.aichner@gmail.com>
2016-03-04 00:52:40 +0100

    Add manifest and service worker for progressive web app.

---
+++ PunchCardFirefoxOs/service-worker.js
@@ -0,0 +1,62 @@
+// Taken from https://railrouter.sg/
+self.addEventListener('install', function(event){
+  console.log('Install');
+});
+
+self.addEventListener('activate', function(event){
+  console.log('Activate');
+  // Calling claim() to force a "controllerchange" event on navigator.serviceWorker
+  event.waitUntil(self.clients.claim());
+});
+
+var cacheName = 'punchcard-cache-v4';
+caches.delete('punchcard-cache-v3'); // Delete the old one
+var successResponses = /^0|([123]\d\d)|(40[14567])|410$/;
+
+function fetchAndCache(request){
+  return fetch(request.clone()).then(function(response){
+    if (request.method == 'GET' && response && successResponses.test(response.status) && (response.type == 'basic' || /\.(js|png|ttf|woff|woff2)/i.test(request.url) || /fonts\.googleapis\.com/i.test(request.url))){
+      console.log('Cache', request.url);
+      caches.open(cacheName).then(function(cache){
+        cache.put(request, response);
+        });
+      }
+    return response;
+    });
+};
+
+function cacheOnly(request){
+  return caches.open(cacheName).then(function(cache){
+    return cache.match(request);
+    });
+};
+
+// Fastest strategy from https://github.com/GoogleChrome/sw-toolbox
+self.addEventListener('fetch', function(event){
+  var request = event.request;
+  var url = request.url;
+  event.respondWith(new Promise(function(resolve, reject){
+    var rejected = false;
+    var reasons = [];
+
+    var maybeReject = function(reason){
+      reasons.push(reason.toString());
+      if (rejected){
+        reject(new Error('Both cache and network failed: "' + reasons.join('", "') + '"'));
+        } else {
+          rejected = true;
+          }
+      };
+
+    var maybeResolve = function(result){
+      if (result instanceof Response){
+        resolve(result);
+        } else {
+          maybeReject('No result returned');
+          }
+      };
+
+    fetchAndCache(request.clone()).then(maybeResolve, maybeReject);
+    cacheOnly(request).then(maybeResolve, maybeReject);
+    }));
+});
